a89037bf47898ab6de4e3aeb4ee4804db11c6067
titus <titusaduket@gmail.com>
2023-10-14 18:01:30 +0300

    test file for amenities

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Tests for amenities
+"""
+import os
+import models
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+class TestAmenity(unittest.TestCase):
+    """
+    Tests for amenities
+    """
+    
+    def test_name(self):
+      """
+      Test for name inputs
+      """
+      pass
+    
+    
+  if __name__ == '__main__'
+      unittest.main()
