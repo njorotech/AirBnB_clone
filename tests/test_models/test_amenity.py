7d0d7133e88e1e701d79cf5be167e90d80b35d2b
titus <titusaduket@gmail.com>
2023-10-15 15:13:09 +0300

    made the amenities nicer

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -8,17 +8,17 @@ import unittest
 from datetime import datetime
 from models.base_model import BaseModel
 
+
 class TestAmenity(unittest.TestCase):
     """
-    Tests for amenities
+     Tests for amenities
     """
-    
+
     def test_name(self):
-      """
-      Test for name inputs
-      """
-      pass
-    
-    
-  if __name__ == '__main__'
-      unittest.main()
+        """
+        Test for name inputs
+        """
+    pass
+
+    if __name__ == '__main__'
+    unittest.main()
